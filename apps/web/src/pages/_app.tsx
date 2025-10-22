bc565d291ca4544c67f85eb4eecf12011ed1e071
Jason Loeffler <j@minorscience.com>
2025-10-22 15:53:44 -0700

    chore(web): [staging] add fonts, no robots

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,14 +1,25 @@
 import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+// Netlify sets CONTEXT to 'branch-deploy' for non-production branches
+const isStaging = process.env.CONTEXT !== 'production';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ContentfulLivePreviewProvider
-      locale="en-US"
-      enableInspectorMode={pageProps.preview}
-      enableLiveUpdates={pageProps.preview}
-    >
-      <Component {...pageProps} />
-    </ContentfulLivePreviewProvider>
+    <>
+      {isStaging && (
+        <Head>
+          <meta name="robots" content="noindex, nofollow" />
+        </Head>
+      )}
+      <ContentfulLivePreviewProvider
+        locale="en-US"
+        enableInspectorMode={pageProps.preview}
+        enableLiveUpdates={pageProps.preview}
+      >
+        <Component {...pageProps} />
+      </ContentfulLivePreviewProvider>
+    </>
   );
 }
