7a0df02df5572df5efd1d7dbc1b8d062ee9af147
Jason Loeffler <j@minorscience.com>
2025-11-19 18:25:17 -0500

    fix(infra): [main] ditching complicated deps

--- apps/web/src/lib/sanity/client.ts
+++ apps/web/src/lib/sanity/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from 'next-sanity';
 
 console.log('===== SANITY ENV CHECK =====');
-console.log('Project ID:', process.env.SANITY_PROJECT_ID);
-console.log('Dataset:', process.env.SANITY_DATASET);
+console.log('Project ID:', process.env.SANITY_STUDIO_PROJECT_ID);
+console.log('Dataset:', process.env.SANITY_STUDIO_DATASET);
 console.log(
   'All env keys:',
   Object.keys(process.env).filter((k) => k.includes('SANITY')),
@@ -11,8 +11,8 @@ console.log(
 console.log('===========================');
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET!,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
   apiVersion: '2024-01-01',
   useCdn: false,
 });
