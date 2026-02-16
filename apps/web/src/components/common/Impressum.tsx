f7d82d662d00a9b188413eab969cbada5a12e03e
Jason Loeffler <j@minorscience.com>
2026-02-16 00:50:55 -0500

    feat(web) home page

--- apps/web/src/components/common/Impressum.tsx
+++ apps/web/src/components/common/Impressum.tsx
@@ -4,9 +4,13 @@ export function Impressum() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="impressum">
-      <p className="impressum-left text">Los Angeles | London</p>
-      <p className="impressum-right text">©{currentYear} Hammer Creative Ltd</p>
-    </div>
+    <>
+      <div className="impressum">
+        <p className="impressum-left text">Los Angeles | London</p>
+        <p className="impressum-right text">
+          ©{currentYear} Hammer Creative Ltd
+        </p>
+      </div>
+    </>
   );
 }
